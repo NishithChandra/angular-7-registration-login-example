1ce99a07ee91933c34bb733930c809c6400d10e7
NishithChandra <nishithch513@gmail.com>
2020-07-11 01:21:39 +0530

    styles for reigstration component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -3,9 +3,13 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
 
+
 import { AlertService, UserService, AuthenticationService } from '@/_services';
 
-@Component({templateUrl: 'register.component.html'})
+@Component({
+    templateUrl: 'register.component.html',
+    styles: ['.jumbForm{ padding-left: 312px;}']
+})
 export class RegisterComponent implements OnInit {
     registerForm: FormGroup;
     loading = false;
